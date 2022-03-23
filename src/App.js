e2f2769177c09460314284de708e26fae5e73bbe
Marijke Drenth <drenth.marijke@googlemail.com>
2022-03-23 16:08:00 +0200

    Add socials

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Info from './components/Info';
 import Section from './components/Sections';
+import SocialsFooter from './components/SocialsFooter';
 import { data } from './data';
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
       <div id="card">
       <Info/>
       {data.map((section) => <Section title={section.title} description={section.description}/> )}
+      <SocialsFooter/>
       </div>
+      
     </div>
   );
 }
---
+++ src/components/SocialsFooter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare} from 'react-icons/fa'
+
+const SocialsFooter = () => {
+    
+    return (
+        <footer>
+            <FaTwitterSquare/>
+            <FaFacebookSquare/>
+            <FaInstagramSquare/>
+            <FaGithubSquare/>
+        </footer>
+    )
+}
+
+export default SocialsFooter
